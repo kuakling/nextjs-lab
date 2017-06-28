f15a9ee6c85b06daa0bc1e3733494023ebb71892
Surakit Choodet <kuakling@gmail.com>
2017-06-28 04:01:28 +0000

    Add react-md

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import flush from 'styled-jsx/server'
+
+export default class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {
+    const {html, head, errorHtml, chunks} = renderPage()
+    const styles = flush()
+    return { html, head, errorHtml, chunks, styles }
+  }
+
+  render () {
+    return (
+      <html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel='stylesheet' href='/static/react-md.min.css' />
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Material+Icons' />
+          <title>Hello NextJs</title>
+        </Head>
+        <body>
+          {this.props.customValue}
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
